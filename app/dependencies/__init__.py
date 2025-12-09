8b577b442714eb8c9beb4a713a78eeaf17061abb
Paulo Guillén <pcguillen@gmail.com>
2025-12-09 18:19:18 -0500

    role_name

--- app/dependencies/__init__.py
+++ app/dependencies/__init__.py
@@ -44,6 +44,42 @@ def is_user_in_role(context, role_name: str):
     except Exception:
         return False
 
+def role_name(context):
+    try:
+        request = context.get('request')
+        if not request:
+            return None
+
+        token = request.cookies.get("access_token")
+        if not token:
+            return None
+
+        settings = get_settings()
+        payload = jwt.decode(token, settings.SECRET_KEY,
+                             algorithms=[settings.ALGORITHM])
+        email = payload.get("sub")
+
+        if not email:
+            return None
+
+        from models.User import Usuario
+        db = next(get_db())
+        try:
+            user = db.query(Usuario).filter(Usuario.email == email).first()
+            if user and user.roles:
+                return [role.name for role in user.roles]
+        finally:
+            db.close()
+
+        return None
+    except Exception:
+        return "" 
+    
+
+    """Devuelve una función que puede ser usada en plantillas Jinja2 para verificar roles"""
+    return is_user_in_role
+              
 
 templates = Jinja2Templates(directory="templates")
 templates.env.globals['is_user_in_role'] = is_user_in_role
+templates.env.globals['role_name'] = role_name
